72cc9334898c21ad514da548634efbbf3a0c7b3c
Celian <celian.colon@polytechnique.org>
2026-04-01 10:53:50 +0200

    release python version constraint

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "License :: Other/Proprietary License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.10,<3.12",
+    python_requires=">=3.10,<3.14",
     install_requires=[
         # Core data processing
         "pandas>=1.3.0,<3.0",
